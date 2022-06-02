57532b593dfc46b745a2a9d82bf47b63fc9e65b2
Mostacz <dawidplewa346@gmail.com>
2022-06-03 01:24:23 +0200

    Brick ballistics

--- PlayerGameObject.js
+++ PlayerGameObject.js
@@ -5,6 +5,9 @@ class PlayerGameObject extends WallGameObject {
         super(posX, posY, width, height, color);
     }
 
+    velocity = 4;
+    acceleration = 1.6;
+
     vector2d = {
         x: 0,
         x_direction: 1,
@@ -13,24 +16,40 @@ class PlayerGameObject extends WallGameObject {
     }
     update(elapsedTime) {
         if(this.engine.inputCollector.key.has("ArrowRight")) {
-            if (this.vector2d.x < 4)
-                this.vector2d.x += 1.6 / elapsedTime;
-            this.vector2d.x_direction = 1;
+            if (this.vector2d.x < this.velocity) {
+                if (this.vector2d.x_direction === -1)
+                    this.vector2d.x -= this.acceleration / elapsedTime;
+                else this.vector2d.x += this.acceleration / elapsedTime;
+            }
+            if (this.vector2d.x <= 0 && this.vector2d.x_direction === -1)
+                this.vector2d.x_direction = 1;
         }
         if(this.engine.inputCollector.key.has("ArrowLeft")) {
-            if (this.vector2d.x < 4)
-                this.vector2d.x += 1.6 / elapsedTime;
-            this.vector2d.x_direction = -1;
+            if (this.vector2d.x < this.velocity) {
+                if (this.vector2d.x_direction === 1)
+                    this.vector2d.x -= this.acceleration / elapsedTime;
+                else this.vector2d.x += this.acceleration / elapsedTime;
+            }
+            if (this.vector2d.x <= 0 && this.vector2d.x_direction === 1)
+                this.vector2d.x_direction = -1;
         }
         if(this.engine.inputCollector.key.has("ArrowUp")) {
-            if (this.vector2d.y < 4)
-                this.vector2d.y += 1.6 / elapsedTime;
-            this.vector2d.y_direction = -1;
+            if (this.vector2d.y < this.velocity) {
+                if (this.vector2d.y_direction === 1)
+                    this.vector2d.y -= this.acceleration / elapsedTime;
+                else this.vector2d.y += this.acceleration / elapsedTime;
+            }
+            if (this.vector2d.y <= 0 && this.vector2d.y_direction === 1)
+                this.vector2d.y_direction = -1;
         }
         if(this.engine.inputCollector.key.has("ArrowDown")) {
-            if (this.vector2d.y < 4)
-                this.vector2d.y += 1.6 / elapsedTime;
-            this.vector2d.y_direction = 1;
+            if (this.vector2d.y < this.velocity) {
+                if (this.vector2d.y_direction === -1)
+                    this.vector2d.y -= this.acceleration / elapsedTime;
+                else this.vector2d.y += this.acceleration / elapsedTime;
+            }
+            if (this.vector2d.y <= 0 && this.vector2d.y_direction === -1)
+                this.vector2d.y_direction = 1;
         }
 
         if (this.vector2d.x > 0)
